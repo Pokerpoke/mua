91b21826e9d4bdb613f5eb9d35fe8bf0c94e5bb7
pokerpoke <pokerpoke@qq.com>
2020-08-12 00:12:23 +0800

    Init

---
+++ src/base/EventLoop.h
@@ -0,0 +1,25 @@
+#ifndef _EVENT_LOOP_H
+#define _EVENT_LOOP_H
+
+namespace mua::base
+{
+    class EventLoop
+    {
+    private:
+        /* data */
+    public:
+        EventLoop(/* args */);
+        ~EventLoop();
+    };
+
+    EventLoop::EventLoop(/* args */)
+    {
+    }
+
+    EventLoop::~EventLoop()
+    {
+    }
+
+} // namespace mua::base
+
+#endif // !_EVENT_LOOP_H
